59e87a45eb3d67bd0fb50ea66be346369a4f5c89
snakeztc <zhaotiancheng.hz@gmail.com>
2016-07-03 00:15:21 -0400

    add new call to uncheck list

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ var view1Controller = app.controller("viewController", function ($scope, $http) 
         $scope.loadingFileData = true;
         $http.get(SERVER_URL + "get/" + $scope.selectedFile).then(function(res){
             $scope.fileData = res.data.data;
+            console.log($scope.fileData.turns);
             $scope.fileData.turns.forEach(function(turn, idx) {
                 $scope.turns.push({
                     idx: idx,
@@ -144,7 +145,7 @@ var view1Controller = app.controller("viewController", function ($scope, $http) 
             } else {
                 var good = true;
                 for (var i = 0; i < turn.actions.length; i++) {
-                    if (turn.actions[i].indexOf('next') > -1) {
+                    if (turn.actions[i].indexOf('next') > -1 || turn.actions[i].indexOf('newcall') > -1) {
                         continue;
                     }
                     var isYielding = $scope.turnYield.indexOf(turn.actions[i]) > -1;
